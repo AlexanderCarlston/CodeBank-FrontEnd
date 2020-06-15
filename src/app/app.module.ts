bcfcec554f69033df41f4201c458c92d7fef71de
AlexanderCarlston <alekscarlston@gmail.com>
2020-06-15 14:34:06 -0600

    Add basic landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { LandingPageComponent } from './features/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent
   ],
   imports: [
     CoreModule,
